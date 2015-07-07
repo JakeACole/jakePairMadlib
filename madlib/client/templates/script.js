8a43c267b0aa7bae8fffecb999a088f791c40d81
Jake <Jakeacole@gmail.com>
2015-07-07 17:23:21 -0400

    Uploading my portion of pair madlib assignment

---
+++ madlib/client/templates/script.js
@@ -0,0 +1,17 @@
+Meteor.startup(function() {
+    $("#submit-btn").click(function() {
+      var exclamation = $("#exclamation").val();
+      var adverb = $("#adverb").val();
+      var noun = $("#noun").val();
+      var verb = $("#verb").val();
+
+      var madlib = "'" + exclamation + "!' he said as he " + 
+      adverb + " jumped onto his " + noun + " and " +
+      verb + " into the sunset.";
+
+      $("mlOutput").hide();
+
+      $("#mlOutput").text(madlib).fadeIn("slow").css ("color","#0000FF");
+
+    });
+});
